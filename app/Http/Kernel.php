5127697e0f022ee4d41c6943130ff0ad21c6d047
Naufal Wahdan <naufalwahdan@gmail.com>
2024-07-25 10:20:06 +0700

    CheckRole done

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];          
+
+    protected $routeMiddleware = [
+        'checkrole' => \App\Http\Middleware\CheckRole::class,
     ];
 }
