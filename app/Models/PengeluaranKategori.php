a9f3374043352fabfd51162bb74540d60c0d315c
Erehac <kumaratioarya@gmail.com>
2024-07-25 13:32:05 +0700

    Models Aset Dan Pengeluaran

---
+++ app/Models/PengeluaranKategori.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PengeluaranKategori extends Model
+{
+    use HasFactory;
+
+    protected $table= 'pengeluaran_kategori';
+
+    protected $fillable= [
+        'nama', 'status',
+    ];
+
+    public function pengeluaran()
+    {
+        return $this->belongsTo(Pengeluaran::class, 'pengeluaran_kategori_id');
+    }
+}
