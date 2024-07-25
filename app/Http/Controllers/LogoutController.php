df4c67f1394921819722c2a1cf0ddc04a829adc6
Erehac <kumaratioarya@gmail.com>
2024-07-25 10:27:04 +0700

    Auth done

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class Logout extends Controller
+{
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
+        ]);
+    }
+}
