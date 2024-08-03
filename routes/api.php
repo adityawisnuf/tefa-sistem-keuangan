02a002aeb16cec6dae02d22a55acc4e129408db3
Fachri Ahmad S <fachrias2107@gmail.com>
2024-08-03 07:39:30 +0700

    menghapus neraca transaksi model dan controller

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\NeracaController;
 
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
@@ -14,9 +13,5 @@ Route::group([
     'middleware' => ['auth:api']
 ], function () {
     Route::post('logout', [LogoutController::class, 'logout']);
-
-    Route::get('/neraca', [NeracaController::class, 'index']);
-    Route::post('/transaksi', [NeracaController::class, 'store']);
-
 
 });
