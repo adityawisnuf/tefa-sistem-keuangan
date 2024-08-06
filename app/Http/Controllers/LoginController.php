01866a097c7a366f6a1f75934b85de0c6724321d
ramarfx <ramagamiing33@gmail.com>
2024-08-06 18:03:02 +0700

    fix: getAllDatas func from master

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -13,16 +14,21 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         //if auth failed
-        if(!$token = auth()->guard('api')->attempt($credentials)) {
+        if (!$token = auth()->guard('api')->attempt($credentials)) {
             return response()->json([
                 'success' => false,
                 'message' => 'Invalid Email or Password.'
             ], 401);
         }
-        
+
         return response()->json([
             'message'   => 'success',
             'token'     => $token
         ]);
+    }
+
+    public function getAllDatas($id)
+    {
+        return User::find($id);
     }
 }
