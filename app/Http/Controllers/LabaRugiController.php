51085ab0c7f96ec697e465b0fce13eac7469849b
RakaMuttaqin <rakamuttaqin13@gmail.com>
2024-08-07 08:46:20 +0700

    penyesuaian filter berdasarkan bulan dan tahun selesai

--- app/Http/Controllers/LabaRugiController.php
+++ app/Http/Controllers/LabaRugiController.php
@@ -12,10 +12,12 @@ class LabaRugiController extends Controller
     public function index(Request $request)
     {
         try {
-            $bulan = $request->input('bulan');
-            $tahun = $request->input('tahun');
+            $bulan = $request->query('bulan');
+            $tahun = $request->query('tahun');
 
-            // Buat tanggal awal dan akhir berdasarkan input
+            // Buat tanggal awal dan akhir berdasarkan query
+            // Jika tidak ada query bulan dan tahun, maka tampilkan data dari bulan dan tahun saat ini
+            $now = Carbon::now();
             $startDate = null;
             $endDate = null;
 
@@ -25,6 +27,9 @@ class LabaRugiController extends Controller
             } elseif ($tahun) {
                 $startDate = Carbon::createFromDate($tahun, 1, 1);
                 $endDate = Carbon::createFromDate($tahun, 12, 31);
+            } elseif (!$bulan && !$tahun) {
+                $startDate = Carbon::createFromDate($now->year, 1, 1);
+                $endDate = Carbon::createFromDate($now->year, 12, 31);
             }
 
             $financialData = $this->retrieveFinancialData($startDate, $endDate);
@@ -58,8 +63,8 @@ class LabaRugiController extends Controller
     private function retrieveFinancialData($startDate, $endDate)
     {
         // Mengambil data Pembayaran dan Pengeluaran
-        $payments = PembayaranSiswa::whereBetween('created_at', [$startDate, $endDate])->get();
-        $expenditures = Pengeluaran::whereBetween('disetujui_pada', [$startDate, $endDate])->get();
+        $payments = PembayaranSiswa::whereBetween('created_at', [$startDate, $endDate])->paginate(20);
+        $expenditures = Pengeluaran::whereBetween('disetujui_pada', [$startDate, $endDate])->paginate(20);
         return [
             'payments' => $payments,
             'expenditures' => $expenditures,
