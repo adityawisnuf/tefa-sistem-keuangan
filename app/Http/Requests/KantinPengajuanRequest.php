9bb683b552608af6bcca8ddb7e01b73b39394d29
Dani Pratama <gleenradarsreal@gmail.com>
2024-08-08 07:46:16 +0700

    FIXED MIGRATIONS KANTIN_PENGAJUAN

---
+++ app/Http/Requests/KantinPengajuanRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class KantinPengajuanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return match ($this->method()) {
+            'POST' => $this->store(),
+            'PUT' => $this->update(),
+        };
+    }
+
+    public function store()
+    {
+        return [
+            'kantin_id' => ['required', 'exists:kantin,id'],
+            'kantin_produk_kategori_id' => ['required', 'exists:kantin,id'],
+            'nama_produk' => ['required', 'string', 'max:255'],
+            'foto_produk' => ['required', 'image', 'mimes:png,jpg,jpeg', 'max:2048'],
+            'deskripsi' => ['required', 'string'],
+            'harga' => ['required', 'integer', 'min:0'],
+            'status' => ['required', 'integer', 'min:0'],
+
+        ];
+    }
+}
