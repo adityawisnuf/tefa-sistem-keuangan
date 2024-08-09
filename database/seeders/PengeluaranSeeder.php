4e3eeda7f6a22b07b6bca0d080644c980b6b71b1
ramarfx <ramagamiing33@gmail.com>
2024-08-09 10:27:37 +0700

    add: pengeluaran seeder

---
+++ database/seeders/PengeluaranSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PengeluaranKategori;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PengeluaranSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $datas = ['gaji guru', 'operasional', 'investasi', 'pembangunan', 'lainnya'];
+
+        foreach ($datas as $data) {
+            PengeluaranKategori::create([
+                'nama' => $data
+            ]);
+        }
+    }
+}
