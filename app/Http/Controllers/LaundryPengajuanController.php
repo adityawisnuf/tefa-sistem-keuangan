d1ae7f2e728350fd1c2bda3b846cc20869bc7a74
RizkiEka <reka48644@gmail.com>
2024-08-13 13:31:27 +0700

    Laundry Pengajuan

--- app/Http/Controllers/LaundryPengajuanController.php
+++ app/Http/Controllers/LaundryPengajuanController.php
@@ -30,4 +30,54 @@ class LaundryPengajuanController extends Controller
             return response()->json(['message' => 'Gagal mengirim pengajuan: ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    public function update(Request $request, LaundryPengajuan $pengajuan) {
+        $request->validate([
+            'status' => 'required|in:pending,disetujui,ditolak'
+        ]);
+
+        $laundry = $pengajuan->laundry;
+
+        if (in_array($pengajuan->status, ['disetujui', 'ditolak'])) {
+            return response()->json([
+                'message' => 'Pengajuan sudah diproses!',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        switch ($pengajuan->status) {
+            case 'pending':
+
+                if ($laundry->saldo >= $pengajuan->jumlah_pengajuan) {
+                    $laundry->saldo -= $pengajuan->jumlah_pengajuan;
+                    $laundry->save();
+
+                    if ($request->status === 'disetujui') {
+                        $pengajuan->update(['status' => 'disetujui']);
+                        return response()->json([
+                            'message' => 'Pengajuan telah disetujui.',
+                            'data' => $pengajuan,
+                        ], Response::HTTP_OK);
+                    } elseif ($request->status === 'ditolak') {
+
+                        $laundry->saldo += $pengajuan->jumlah_pengajuan;
+                        $laundry->save();
+                        $pengajuan->update(['status' => 'ditolak']);
+                        return response()->json([
+                            'message' => 'Pengajuan telah ditolak dan saldo dikembalikan.',
+                            'data' => $pengajuan,
+                        ], Response::HTTP_OK);
+                    }
+                } else {
+                    return response()->json([
+                        'message' => 'Saldo tidak mencukupi untuk pengajuan ini.',
+                    ], Response::HTTP_BAD_REQUEST);
+                }
+                break;
+
+            default:
+                return response()->json([
+                    'message' => 'Status tidak valid.',
+                ], Response::HTTP_UNAUTHORIZED);
+        }
+    }
 }
