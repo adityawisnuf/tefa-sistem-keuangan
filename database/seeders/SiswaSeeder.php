81f9506e5d0f97cc045275c384acd092b8337bec
RakaMuttaqin <rakamuttaqin13@gmail.com>
2024-08-15 10:14:42 +0700

    Seeder untuk dummy data

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -17,19 +17,34 @@ class SiswaSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        for ($i = 0; $i < 10; $i++) {
-            Siswa::create([
-                'user_id' =>1, // Replace with actual user IDs
-                'nama_depan' => $faker->firstName(),
-                'nama_belakang' => $faker->lastName(),
+        $siswa = [
+            [
+                'user_id' => 6, // Replace with actual user IDs
+                'nama_depan' => 'Ahmad',
+                'nama_belakang' => 'Fauzi',
                 'alamat' => $faker->address(),
                 'tempat_lahir' => $faker->city(),
                 'tanggal_lahir' => $faker->dateTimeBetween('-18 years')->format('Y-m-d'),
                 'telepon' => $faker->phoneNumber(),
-                'kelas_id' =>1,
+                'kelas_id' => 1,
                 'orangtua_id' => 1,
                 'village_id' => rand(1, 10),
-            ]);
+            ],
+            [
+                'user_id' => 7, // Replace with actual user IDs
+                'nama_depan' => 'Dewi',
+                'nama_belakang' => 'Lestari',
+                'alamat' => $faker->address(),
+                'tempat_lahir' => $faker->city(),
+                'tanggal_lahir' => $faker->dateTimeBetween('-18 years')->format('Y-m-d'),
+                'telepon' => $faker->phoneNumber(),
+                'kelas_id' => 1,
+                'orangtua_id' => 1,
+                'village_id' => rand(1, 10),
+            ],
+        ];
+        foreach ($siswa as $data) {
+            Siswa::create($data);
         }
     }
 }
