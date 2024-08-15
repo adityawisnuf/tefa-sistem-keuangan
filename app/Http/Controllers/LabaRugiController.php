6219b8e566c7bfc5c49d546ec598f29675e3bbdc
Fachri Ahmad S <fachrias2107@gmail.com>
2024-08-15 11:36:36 +0700

    mengkelompokan kategori untuk labarugi

--- app/Http/Controllers/LabaRugiController.php
+++ app/Http/Controllers/LabaRugiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\PembayaranPpdb;
 use App\Models\PembayaranSiswa;
 use App\Models\Pengeluaran;
+use App\Models\PengeluaranKategori;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -19,11 +20,10 @@ class LabaRugiController extends Controller
             $bulan = $request->query('bulan');
             $tahun = $request->query('tahun');
 
-            //default (no parameter), if month only, if year & month
+            // Set tanggal mulai dan akhir
             $this->startDate = Carbon::createFromDate($tahun ?? date('Y'), $bulan ?? date('m'), 1);
             $this->endDate = $this->startDate->copy()->endOfMonth();
 
-            //if year only (ngeri bosque)
             if ($tahun && !$bulan) {
                 $this->startDate = Carbon::createFromDate($tahun, 1, 1);
                 $this->endDate = Carbon::createFromDate($tahun, 12, 31);
@@ -32,25 +32,30 @@ class LabaRugiController extends Controller
             $financialData = $this->retrieveFinancialData();
             $financialMetrics = $this->calculateFinancialMetrics($financialData);
 
-            // Mengubah pengeluaran menjadi array
-            $pengeluaranArray = $financialData['expenditures']->map(function ($item) {
-                return [
-                    'keperluan' => $item->keperluan,
-                    'nominal' => $item->nominal,
-                ];
-            })->toArray();
+            // Ambil kategori pengeluaran
+            $categories = PengeluaranKategori::pluck('nama', 'id')->toArray();
 
+            // Kelompokkan pengeluaran berdasarkan kategori
+            $pengeluaranByCategory = $financialData['expenditures']->groupBy('pengeluaran_kategori_id')->map(function ($items) use ($categories) {
+                $kategoriId = $items->first()->pengeluaran_kategori_id;
+                return [
+                    'keperluan' => $categories[$kategoriId] ?? 'Tidak Diketahui',
+                    'nominal' => $items->sum('nominal')
+                ];
+            })->values();
+
+            // Hitung rasio laba bersih dan beban operasional
             $rasioLabaBersih = $this->formatPercentage($financialMetrics['profit'], $financialMetrics['totalPayment']);
             $rasioBeban = $this->formatPercentage($financialMetrics['totalExpenditure'], $financialMetrics['totalPayment']);
 
             // Jika tidak ada data, kembalikan data kosong
-            if (!$financialMetrics['totalPayment'] && empty($pengeluaranArray)) {
+            if (!$financialMetrics['totalPayment'] && $pengeluaranByCategory->isEmpty()) {
                 return response()->json(['data' => []], 200);
             }
 
             $data = [
                 'pendapatan' => $financialMetrics['totalPayment'],
-                'pengeluaran' => $pengeluaranArray,
+                'pengeluaran' => $pengeluaranByCategory,
                 'pengeluaran_total' => $financialMetrics['totalExpenditure'],
                 'laba_bersih' => $financialMetrics['profit'],
                 'rasio_laba_bersih' => $rasioLabaBersih,
@@ -69,9 +74,12 @@ class LabaRugiController extends Controller
     private function retrieveFinancialData()
     {
         // Mengambil data Pembayaran dan Pengeluaran
-        $payments = PembayaranSiswa::whereBetween('created_at', [$this->startDate, $this->endDate])->where('status', 1);
-        $paymentsPpdb = PembayaranPpdb::whereBetween('created_at', [$this->startDate, $this->endDate])->where('status', 1);
+        $payments = PembayaranSiswa::whereBetween('created_at', [$this->startDate, $this->endDate])
+            ->where('status', 1);
+        $paymentsPpdb = PembayaranPpdb::whereBetween('created_at', [$this->startDate, $this->endDate])
+            ->where('status', 1);
         $expenditures = Pengeluaran::whereBetween('disetujui_pada', [$this->startDate, $this->endDate])->get();
+
         return [
             'payments' => $payments,
             'paymentsPpdb' => $paymentsPpdb,
@@ -81,14 +89,14 @@ class LabaRugiController extends Controller
 
     private function calculateFinancialMetrics(array $financialData)
     {
-        // Hitung laba kotor, total pengeluaran, dan laba bersih
         $totalPayment = $financialData['payments']->sum('nominal') + $financialData['paymentsPpdb']->sum('nominal');
-        // $totalExpenditure = Pengeluaran::whereBetween('disetujui_pada', [$this->startDate, $this->endDate])->sum('nominal');
         $totalExpenditure = $financialData['expenditures']->sum('nominal');
         $profit = $totalPayment - $totalExpenditure;
+
         if ($profit < 0) {
             $profit = 0;
         }
+
         return [
             'totalPayment' => $totalPayment,
             'totalExpenditure' => $totalExpenditure,
@@ -98,11 +106,10 @@ class LabaRugiController extends Controller
 
     private function formatPercentage($value, $divisor)
     {
-        
-        // biar keren
         if ($divisor) return ($value / $divisor) * 100;
         return $value > 0 ? 100.0 : 0.0;
     }
+
     public function getOptions()
     {
         $data = PembayaranSiswa::selectRaw('DISTINCT YEAR(created_at) as year, MONTHNAME(created_at) as month')
