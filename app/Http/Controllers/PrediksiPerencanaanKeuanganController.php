fff6d62e5958a7f31f47e1c3be6a8d9999b9679a
Fachri Ahmad S <fachrias2107@gmail.com>
2024-08-22 11:57:05 +0700

    revisi prediksi dan rasio filtering sudah jalan

--- app/Http/Controllers/PrediksiPerencanaanKeuanganController.php
+++ app/Http/Controllers/PrediksiPerencanaanKeuanganController.php
@@ -11,24 +11,25 @@ class PrediksiPerencanaanKeuanganController extends Controller
     {
         $data = $this->retrieveData();
 
-        return response()->json([
-            'data' => $data,
-        ], 200);
+        return response()->json(['data' => $data], 200);
     }
 
     private function retrieveData()
     {
-        // Retrieve data from the database using eager loading for efficiency
-        $anggaran = Anggaran::get();
+        // Retrieve all data from the database
+        $anggaran = Anggaran::all();
 
-        // Group the data based on status
-        $data = [
-            'diajukan' => $anggaran->where('status', 1),
-            'diapprove' => $anggaran->where('status', 2),
-            'terealisasikan' => $anggaran->where('status', 3),
-            'gagal' => $anggaran->where('status', 4),
+        // Remove the 'id' field from the result
+        $anggaranFiltered = $anggaran->map(function ($item) {
+            return $item->makeHidden('id', 'deskripsi', 'created_at', 'updated_at');
+        });
+
+        return [
+            'anggaran' => $anggaranFiltered,
+            'count_diajukan' => $anggaran->where('status',1)->count(),
+            'count_diapprove' => $anggaran->where('status',2)->count(),
+            'count_terealisasikan' => $anggaran->where('status',3)->count(),
+            'count_gagal' => $anggaran->where('status',4)->count(),
         ];
-
-        return $data;
     }
 }
--- app/Http/Controllers/RasioKeuanganController.php
+++ app/Http/Controllers/RasioKeuanganController.php
@@ -10,7 +10,6 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
-
 class RasioKeuanganController extends Controller
 {
     public function index(Request $request)
@@ -24,17 +23,21 @@ class RasioKeuanganController extends Controller
             ->when($bulan, fn($query) => $query->whereMonth('created_at', $bulan))
             ->when($tahun, fn($query) => $query->whereYear('created_at', $tahun))
             ->sum('nominal');
+
         $paymentsPpdb = PembayaranPpdb::where('status', 1)
             ->when($bulan, fn($query) => $query->whereMonth('created_at', $bulan))
             ->when($tahun, fn($query) => $query->whereYear('created_at', $tahun))
             ->sum('nominal');
+
         $asset = AsetSekolah::when($bulan, fn($query) => $query->whereMonth('created_at', $bulan))
             ->when($tahun, fn($query) => $query->whereYear('created_at', $tahun))
             ->sum('harga');
+
         $asetTetap = AsetSekolah::where('tipe', 'tetap') // Filter berdasarkan tipe aset
             ->when($bulan, fn($query) => $query->whereMonth('created_at', $bulan))
             ->when($tahun, fn($query) => $query->whereYear('created_at', $tahun))
             ->sum('harga');
+
         $asetLancar = AsetSekolah::where('tipe', 'lancar') // Filter berdasarkan tipe aset
             ->when($bulan, fn($query) => $query->whereMonth('created_at', $bulan))
             ->when($tahun, fn($query) => $query->whereYear('created_at', $tahun))
@@ -45,9 +48,10 @@ class RasioKeuanganController extends Controller
             ->when($bulan, fn($query) => $query->whereMonth('disetujui_pada', $bulan))
             ->when($tahun, fn($query) => $query->whereYear('disetujui_pada', $tahun))
             ->sum('nominal');
+
         $totalLiability = Pengeluaran::whereNull('disetujui_pada')
-            ->when($bulan, fn($query) => $query->whereMonth('disetujui_pada', $bulan))
-            ->when($tahun, fn($query) => $query->whereYear('disetujui_pada', $tahun))
+            ->when($bulan, fn($query) => $query->whereMonth('diajukan_pada', $bulan))
+            ->when($tahun, fn($query) => $query->whereYear('diajukan_pada', $tahun))
             ->sum('nominal');
 
         $currentLiability = DB::table('pengeluaran')
@@ -58,7 +62,6 @@ class RasioKeuanganController extends Controller
             ->when($tahun, fn($query) => $query->whereYear('pengeluaran.created_at', $tahun))
             ->sum('pengeluaran.nominal');
 
-
         $inventory = DB::table('pengeluaran')
             ->join('pengeluaran_kategori', 'pengeluaran.pengeluaran_kategori_id', '=', 'pengeluaran_kategori.id')
             ->where('pengeluaran_kategori.nama', 'Barang Habis Pakai')
@@ -68,9 +71,6 @@ class RasioKeuanganController extends Controller
 
         $equity = $asset - $totalLiability;
 
-
-
-
         // Menghitung total pemasukan
         $totalPayment = $payments + $paymentsPpdb;
 
@@ -78,88 +78,63 @@ class RasioKeuanganController extends Controller
         $profit = $totalPayment - $expenses;
 
         // Menghitung rasio
-        $cr = $this->currentRatio($asetLancar, $currentLiability) ;
+        $cr = $this->currentRatio($asetLancar, $currentLiability);
         $qr = $this->quickRatio($asetLancar, $inventory, $currentLiability);
-        $npm = $this->netProfitMargin($profit, $totalPayment)*100;
-        $roa = $this->returnOnAsset($profit, $asset)*100;
-        $oer = $this->operatingExpenseRatio($expenses ,$totalPayment)*100;
+        $npm = $this->netProfitMargin($profit, $totalPayment) * 100;
+        $roa = $this->returnOnAsset($profit, $asset) * 100;
+        $oer = $this->operatingExpenseRatio($expenses, $totalPayment) * 100;
         $toa = $this->turnoverAsset($totalPayment, $asetTetap);
-        $dter = $this->debtToEquityRatio($asset,$equity);
-        $dr = $this->debtRatio($totalLiability,$asset)*100;
+        $dter = $this->debtToEquityRatio($totalLiability, $equity);
+        $dr = $this->debtRatio($totalLiability, $asset)*100;
 
-
-        // Menyiapkan data yang akan dikembalikan
-        $data = [
+        return response()->json([
             'current_ratio' => $cr,
             'quick_ratio' => $qr,
             'net_profit_margin' => $npm,
-            'return_on_asset' => $roa,
+            'return_on_assets' => $roa,
             'operating_expense_ratio' => $oer,
-            'turnover_aset' => $toa,
+            'turnover_of_assets' => $toa,
             'debt_to_equity_ratio' => $dter,
             'debt_ratio' => $dr,
-        ];
-
-        return response()->json(['data' => $data], 200);
+        ]);
     }
 
-    //Rasio Profitabilitas
+    private function currentRatio($asetLancar, $currentLiabilities)
+    {
+        return $currentLiabilities  == 0 ? 0 : $asetLancar / $currentLiabilities;
+    }
+
+    private function quickRatio($asetLancar, $inventory, $currentLiabilities)
+    {
+        return $currentLiabilities == 0 ? 0 : ($asetLancar - $inventory) / $currentLiabilities;
+    }
+
     private function netProfitMargin($profit, $totalPayment)
     {
-        if ($totalPayment == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return ($profit / $totalPayment) ;
-    }
-    private function returnOnAsset($profit, $asset)
-    {
-        if ($asset == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return ($profit / $asset);
+        return $totalPayment   == 0 ? 0 : $profit / $totalPayment;
     }
 
-    //Rasio efesiensi
-    private function turnoverAsset($totalPayment, $asetTetap)
+    private function returnOnAsset($profit, $asset)
     {
-        if ($asetTetap == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return $totalPayment / $asetTetap;
+        return $asset  == 0 ? 0 : $profit / $asset;
     }
 
     private function operatingExpenseRatio($expenses, $totalPayment)
     {
-        if ($totalPayment == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return $expenses / $totalPayment ;
+        return $totalPayment  == 0 ? 0 : $expenses / $totalPayment;
     }
 
-    //Rasio likuiditas
-    private function currentRatio($asetLancar, $currentLiability){
-        if ($currentLiability == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return $asetLancar / $currentLiability;
+    private function turnoverAsset($totalPayment, $fixedAssets)
+    {
+        return $fixedAssets  == 0 ? 0 : $totalPayment / $fixedAssets;
     }
-    private function quickRatio($asetLancar,$inventory, $currentLiability){
-        if ($currentLiability == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return ($asetLancar - $inventory) / $currentLiability;
+
+    private function debtToEquityRatio($totalLiabilities, $equity)
+    {
+        return $equity  == 0 ? 0 : $totalLiabilities / $equity;
     }
-    //Rasio solvabilitas
-    private function debtToEquityRatio($totalLiability, $equity){
-        if ($equity == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return $totalLiability / $equity;
-    }
-    private function debtRatio($totalLiability, $asset){
-        if ($asset == 0) {
-            return 'N/A'; // Menghindari pembagian dengan nol
-        }
-        return $totalLiability / $asset;
+    private function debtRatio($totalLiabilities, $asset)
+    {
+        return $asset  == 0 ? 0 : $totalLiabilities / $asset;
     }
 }
