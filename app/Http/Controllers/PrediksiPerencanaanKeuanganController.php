fef064037c0d6e6d64e26157f6dac15a69a09f44
RakaMuttaqin <rakamuttaqin13@gmail.com>
2024-08-22 08:26:03 +0700

    memulai prediksi dan perencanaan keuangan

--- app/Http/Controllers/PrediksiPerencanaanKeuanganController.php
+++ app/Http/Controllers/PrediksiPerencanaanKeuanganController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Anggaran;
 use Illuminate\Http\Request;
 
 class PrediksiPerencanaanKeuanganController extends Controller
 {
-    public function index(){
-        return "halo rie ganteng!";
+    public function index()
+    {
+        $data = $this->retrieveData();
 
-        
+        return response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    private function retrieveData()
+    {
+        // Retrieve data from the database using eager loading for efficiency
+        $anggaran = Anggaran::get();
+
+        // Group the data based on status
+        $data = [
+            'diajukan' => $anggaran->where('status', 1),
+            'diapprove' => $anggaran->where('status', 2),
+            'terealisasikan' => $anggaran->where('status', 3),
+            'gagal' => $anggaran->where('status', 4),
+        ];
+
+        return $data;
     }
 }
