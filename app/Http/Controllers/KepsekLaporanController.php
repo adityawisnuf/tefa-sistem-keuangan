04bda3ae6ce05029b42a9103f7fc948c6bc92ac3
pisangkeju <satria.ndoy1710@gmail.com>
2024-08-26 23:13:01 +0700

    kepsek done (lupa commit lagi)

--- app/Http/Controllers/KepsekLaporanController.php
+++ app/Http/Controllers/KepsekLaporanController.php
@@ -13,8 +13,8 @@ class KepsekLaporanController extends Controller
     public function getUsahaTransaksi()
     {
         $validator = Validator::make(request()->all(), [
-            'tahun' => ['nullable', 'integer', 'min:1900', 'max:' . Carbon::now()->year],
-            'bulan' => ['nullable', 'integer', 'min:1', 'max:12'],
+            'tanggal_awal' => ['nullable', 'date'],
+            'tanggal_akhir' => ['nullable', 'date', 'after_or_equal:tanggal_awal'],
             'per_page' => ['nullable', 'integer', 'min:1'],
             'role' => ['nullable', 'in:Kantin,Laundry'],
             'nama_usaha' => ['nullable', 'string'],
@@ -24,34 +24,57 @@ class KepsekLaporanController extends Controller
             return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
         }
 
-        $year = request('tahun', Carbon::now()->year);
-        $month = request('bulan', Carbon::now()->month);
-        $nama_usaha = request('nama_usaha', null);
+        $startDate = request('tanggal_awal');
+        $endDate = request('tanggal_akhir');
+        $nama_usaha = request('nama_usaha');
         $role = request('role', 'Kantin');
         $perPage = request('per_page', 10);
-
-        $startDate = Carbon::create($year, $month, 1);
-        $endDate = $startDate->copy()->endOfMonth();
 
         $model = $role == 'Kantin' ? new KantinTransaksi : new LaundryTransaksi;
 
         try {
             $transaksi = $model->with([
                 $role == 'Kantin'
-                ? 'kantin_transaksi_detail.kantin_produk'
-                : 'laundry_transaksi_detail.laundry_layanan',
-                'usaha:id,nama_usaha'
+                ? 'kantin_transaksi_detail.kantin_produk:id,nama_produk,foto_produk,deskripsi'
+                : 'laundry_transaksi_detail.laundry_layanan:id,nama_layanan,foto_layanan,deskripsi',
+                'usaha:id,nama_usaha',
+                'siswa:id,nama_depan,nama_belakang'
             ])
-                ->whereBetween('tanggal_selesai', [$startDate, $endDate])
+                ->when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
+                    $query->whereBetween('tanggal_selesai', [
+                        Carbon::parse($startDate)->startOfDay(),
+                        Carbon::parse($endDate)->endOfDay()
+                    ]);
+                }, function ($query) {
+                    $query->whereBetween('tanggal_selesai', [
+                        Carbon::now()->startOfMonth(),
+                        Carbon::now()->endOfMonth()
+                    ]);
+                })
                 ->whereIn('status', ['selesai', 'dibatalkan'])
                 ->when($nama_usaha, function ($query) use ($nama_usaha) {
-                    $query->whereRelation('usaha', 'nama_usaha', 'like', '%' . $nama_usaha . '%');
+                    $query->whereRelation('usaha', 'nama_usaha', 'like', "%$nama_usaha%");
                 })
                 ->paginate($perPage);
-    
+
+            $transaksi->getCollection()->transform(function ($transaksi) use ($role) {
+                return [
+                    'id' => $transaksi->id,
+                    'nama_siswa' => "{$transaksi->siswa->nama_depan} {$transaksi->siswa->nama_belakang}",
+                    'nama_usaha' => $transaksi->usaha->nama_usaha,
+                    'status' => $transaksi->status,
+                    'tanggal_pemesanan' => $transaksi->tanggal_pemesanan,
+                    'tanggal_selesai' => $transaksi->tanggal_selesai,
+                    'transaksi_detail' => $role == 'Kantin'
+                        ? $transaksi->kantin_transaksi_detail
+                        : $transaksi->laundry_transaksi_detail
+                ];
+            });
+
             return response()->json(['data' => $transaksi], Response::HTTP_OK);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Terjadi kesalahan saat mengambil data transaksi.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            // return response()->json(['error' => 'Terjadi kesalahan saat mengambil data transaksi: ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
--- app/Http/Controllers/KepsekPengajuanController.php
+++ app/Http/Controllers/KepsekPengajuanController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\UsahaPengajuan;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,8 +12,8 @@ class KepsekPengajuanController extends Controller
     public function getUsahaPengajuan()
     {
         $validator = Validator::make(request()->all(), [
-            'tahun' => ['nullable', 'integer', 'min:1900', 'max:' . Carbon::now()->year],
-            'bulan' => ['nullable', 'integer', 'min:1', 'max:12'],
+            'tanggal_awal' => ['nullable', 'date'],
+            'tanggal_akhir' => ['nullable', 'date', 'after_or_equal:tanggal_awal'],
             'per_page' => ['nullable', 'integer', 'min:1'],
             'role' => ['nullable', 'in:Kantin,Laundry'],
             'nama_usaha' => ['nullable', 'string'],
@@ -24,36 +23,48 @@ class KepsekPengajuanController extends Controller
             return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
         }
 
-        $year = request('tahun', Carbon::now()->year);
-        $month = request('bulan', Carbon::now()->month);
+        $startDate = request('tanggal_awal');
+        $endDate = request('tanggal_akhir');
         $nama_usaha = request('nama_usaha');
         $role = request('role', 'Kantin');
         $perPage = request('per_page', 10);
 
-        $startDate = Carbon::create($year, $month, 1);
-        $endDate = $startDate->copy()->endOfMonth();
+        try {
+            $pengajuan = UsahaPengajuan::with(['usaha.user'])
+                ->when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
+                    $query->whereBetween('tanggal_selesai', [
+                        Carbon::parse($startDate)->startOfDay(),
+                        Carbon::parse($endDate)->endOfDay()
+                    ]);
+                }, function ($query) {
+                    $query->whereBetween('tanggal_selesai', [
+                        Carbon::now()->startOfMonth(),
+                        Carbon::now()->endOfMonth()
+                    ]);
+                })
+                ->whereIn('status', ['disetujui', 'ditolak'])
+                ->whereRelation('usaha.user', 'role', 'like', "%$role%")
+                ->when($nama_usaha, function ($query) use ($nama_usaha) {
+                    $query->whereRelation('usaha', 'nama_usaha', 'like', "%$nama_usaha%");
+                })
+                ->paginate($perPage);
 
-        $pengajuan = UsahaPengajuan::with(['usaha.user'])
-            ->whereBetween('tanggal_selesai', [$startDate, $endDate])
-            ->whereIn('status', ['disetujui', 'ditolak'])
-            ->whereRelation('usaha.user', 'role', 'like', "%$role%")
-            ->when($nama_usaha, function ($query) use ($nama_usaha) {
-                $query->whereRelation('usaha', 'nama_usaha','like', "%$nama_usaha%")
-            })
-            ->paginate($perPage);
+            $pengajuan->getCollection()->transform(function ($pengajuan) {
+                return [
+                    'id' => $pengajuan->id,
+                    'nama_usaha' => $pengajuan->usaha->nama_usaha,
+                    'jumlah_pengajuan' => $pengajuan->jumlah_pengajuan,
+                    'status' => $pengajuan->status,
+                    'alasan_penolakan' => $pengajuan->alasan_penolakan,
+                    'tanggal_pengajuan' => $pengajuan->tanggal_pengajuan,
+                    'tanggal_selesai' => $pengajuan->tanggal_selesai,
+                ];
+            });
 
-        $pengajuan->getCollection()->transform(function ($pengajuan) {
-            return [
-                'id' => $pengajuan->id,
-                'nama_usaha' => $pengajuan->usaha->nama_usaha,
-                'jumlah_pengajuan' => $pengajuan->jumlah_pengajuan,
-                'status' => $pengajuan->status,
-                'alasan_penolakan' => $pengajuan->alasan_penolakan,
-                'tanggal_pengajuan' => $pengajuan->tanggal_pengajuan,
-                'tanggal_selesai' => $pengajuan->tanggal_selesai,
-            ];
-        });
+            return response()->json(['data' => $pengajuan], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Terjadi kesalahan saat mengambil data pengajuan.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
 
-        return response()->json(['data' => $pengajuan], Response::HTTP_OK);
     }
 }
