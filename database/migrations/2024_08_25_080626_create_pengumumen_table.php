7b4120da0e22ae227de5b7ff7c8912d6972c7b0d
awiipp <sayidariipp@gmail.com>
2024-08-27 19:54:40 +0700

    add pengumuman model and migration

---
+++ database/migrations/2024_08_25_080626_create_pengumumen_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengumuman', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('status')->default(1); // 1=diajukan, 2=diapprove, 3=direject
+            $table->string('judul');
+            $table->text('isi');
+            $table->text('pesan_ditolak')->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengumuman');
+    }
+};
