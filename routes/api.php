d1739e6d24c24a27feca9e7ef81ded81de576d5b
ramarfx <ramagamiing33@gmail.com>
2024-09-26 12:02:20 +0700

    fix: routing

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KelasController;
@@ -15,17 +16,11 @@ use App\Http\Controllers\PembayaranDuitkuController;
 use App\Http\Controllers\PembayaranKategoriController;
 use App\Http\Controllers\PembayaranSiswaCicilanController;
 use App\Http\Controllers\PengeluaranAnalysis;
-use App\Http\Controllers\OrangTuaController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\PengeluaranController;
-use App\Http\Controllers\SekolahController;
-use App\Http\Controllers\SiswaController;
-use App\Http\Controllers\KelasController;
 use App\Http\Controllers\PengumumanController;
 use App\Http\Controllers\PengeluaranKategoriController;
 
 // ROLE : Admin; KepalaSekolah; Bendahara; OrangTua; Siswa; Kantin; Laundry;
-  
+
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 
@@ -35,45 +30,7 @@ Route::group(['middleware' => ['auth:api']], function () {
 
     // pengeluaran
     Route::post('pengeluaran/kategori', [PengeluaranController::class, 'addPengeluaranKategori']);
-
-
-Route::get('orangtua', [OrangTuaController::class, 'getAllSekolah']);
-Route::get('orangtua/{id}', [OrangTuaController::class, 'show']);
-Route::post('orangtua', [OrangTuaController::class, 'store']);
-Route::patch('orangtua/{id}', [OrangTuaController::class, 'update']);
-Route::delete('orangtua/{id}', [OrangTuaController::class, 'destroy']);
-
-
-Route::post('sekolah', [SekolahController::class, 'store']);
-
-// get sekolah
-Route::get('sekolah', [SekolahController::class, 'getAllSekolah']);
-
-// update sekolah
-Route::put('/sekolah/{id}', [SekolahController::class, 'update']);
-
-// delete sekolah
-Route::delete('/sekolah/{id}', [SekolahController::class, 'destroy']);
-Route::get('sekolah/{id}', [SekolahController::class, 'show']);
-
-
-// kelas crud
-Route::get('kelas', [KelasController::class, 'index']);
-Route::get('kelas/{id}', [KelasController::class, 'show']);
-Route::post('kelas', [KelasController::class, 'store']);
-Route::put('kelas/{id}', [KelasController::class, 'update']);
-Route::delete('kelas/{id}', [KelasController::class, 'destroy']);
-
-
-
-// data siswa
-Route::get('siswa', [SiswaController::class, 'getAllSiswa']);
-Route::get('siswa/{id}', [SiswaController::class, 'show']);
-Route::post('siswa', [SiswaController::class, 'store']);
-Route::put('siswa/{id}', [SiswaController::class, 'updateSiswa']);
-Route::delete('siswa/{id}', [SiswaController::class, 'destroy']);
-// close data siswa
-
+});
 
 // pengumuman
 Route::middleware(['auth:api'])->group(function () {
