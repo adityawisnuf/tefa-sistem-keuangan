be01cc9434a87790aef88dd61cb0970c8a96ba80
pisangkeju <satria.ndoy1710@gmail.com>
2024-10-05 02:51:05 +0700

    kepsekPengajuan last

--- app/Http/Controllers/KepsekPengajuanController.php
+++ app/Http/Controllers/KepsekPengajuanController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\UsahaPengajuan;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class KepsekPengajuanController extends Controller
 {
-    public function getUsahaPengajuan()
+    public function getUsahaPengajuan(Request $request)
     {
-        $validator = Validator::make(request()->all(), [
+        $validated = $request->validate([
             'tanggal_awal' => ['nullable', 'date'],
             'tanggal_akhir' => ['nullable', 'date', 'after_or_equal:tanggal_awal'],
             'per_page' => ['nullable', 'integer', 'min:1'],
@@ -20,52 +19,38 @@ class KepsekPengajuanController extends Controller
             'nama_usaha' => ['nullable', 'string'],
         ]);
 
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
-        }
+        $startDate = $validated['tanggal_awal'] ?? null;
+        $endDate = $validated['tanggal_akhir'] ?? null;
+        $nama_usaha = $validated['nama_usaha'] ?? null;
+        $role = $validated['role'] ?? 'Kantin';
+        $perPage = $validated['per_page'] ?? 10;
 
-        $startDate = request('tanggal_awal');
-        $endDate = request('tanggal_akhir');
-        $nama_usaha = request('nama_usaha');
-        $role = request('role', 'Kantin');
-        $perPage = request('per_page', 10);
+        $pengajuan = UsahaPengajuan
+            ::select('id', 'usaha_id', 'jumlah_pengajuan', 'status', 'alasan_penolakan', 'tanggal_pengajuan', 'tanggal_selesai')
+            ->with(
+                'usaha:id,user_id,nama_usaha',
+                'usaha.user:id,role'
+            )
+            ->when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
+                $query->whereBetween('tanggal_selesai', [
+                    Carbon::parse($startDate)->startOfDay(),
+                    Carbon::parse($endDate)->endOfDay()
+                ]);
+            })
+            ->whereIn('status', ['disetujui', 'ditolak'])
+            ->whereRelation('usaha.user', 'role', 'like', "%$role%")
+            ->when($nama_usaha, function ($query) use ($nama_usaha) {
+                $query->whereRelation('usaha', 'nama_usaha', 'like', "%$nama_usaha%");
+            })
+            ->paginate($perPage);
 
-        try {
-            $pengajuan = UsahaPengajuan::with(['usaha.user'])
-                ->when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
-                    $query->whereBetween('tanggal_selesai', [
-                        Carbon::parse($startDate)->startOfDay(),
-                        Carbon::parse($endDate)->endOfDay()
-                    ]);
-                }, function ($query) {
-                    $query->whereBetween('tanggal_selesai', [
-                        Carbon::now()->startOfMonth(),
-                        Carbon::now()->endOfMonth()
-                    ]);
-                })
-                ->whereIn('status', ['disetujui', 'ditolak'])
-                ->whereRelation('usaha.user', 'role', 'like', "%$role%")
-                ->when($nama_usaha, function ($query) use ($nama_usaha) {
-                    $query->whereRelation('usaha', 'nama_usaha', 'like', "%$nama_usaha%");
-                })
-                ->paginate($perPage);
+        $pengajuan->getCollection()->transform(function ($pengajuan) {
+            return array_merge(
+                collect($pengajuan)->forget('usaha')->toArray(),
+                ['nama_usaha' => $pengajuan->usaha->nama_usaha]
+            );
+        });
 
-            $pengajuan->getCollection()->transform(function ($pengajuan) {
-                return [
-                    'id' => $pengajuan->id,
-                    'nama_usaha' => $pengajuan->usaha->nama_usaha,
-                    'jumlah_pengajuan' => $pengajuan->jumlah_pengajuan,
-                    'status' => $pengajuan->status,
-                    'alasan_penolakan' => $pengajuan->alasan_penolakan,
-                    'tanggal_pengajuan' => $pengajuan->tanggal_pengajuan,
-                    'tanggal_selesai' => $pengajuan->tanggal_selesai,
-                ];
-            });
-
-            return response()->json(['data' => $pengajuan], Response::HTTP_OK);
-        } catch (\Exception $e) {
-            Log::error('getUsahaTransaksi: ' . $e->getMessage());
-            return response()->json(['error' => 'Terjadi kesalahan saat mengambil data pengajuan.'], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        return response()->json(['data' => $pengajuan], Response::HTTP_OK);
     }
 }
