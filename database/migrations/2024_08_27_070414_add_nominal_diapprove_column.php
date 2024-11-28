fe62e90478f037ed4e6c1925cb5074fa3b452270
Elmokucho <elmokuchoo@gmail.com>
2024-11-28 16:58:44 +0700

    fix unsolved conflict

--- database/migrations/2024_08_27_070414_add_nominal_diapprove_column.php
+++ database/migrations/2024_08_27_070414_add_nominal_diapprove_column.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<< HEAD:database/migrations/2024_08_07_122729_update_aset_add_nominal_and_type.php
-        Schema::table('aset', function (Blueprint $table) {
-            $table->string('tipe', 255)->after('id')->nullable();
-            $table->double('harga', 10, 2)->after('nama')->default(0);
-=======
         Schema::table('anggaran', function (Blueprint $table) {
             $table->integer('nominal_diapprove')->nullable()->after('nominal');
->>>>>>> 06c349edbca295a5c90b039e83feff5ff13cfa67:database/migrations/2024_08_27_070414_add_nominal_diapprove_column.php
         });
     }
 
