0922425d1fc03d199fbfb2df0a58ecfb2434833c
Elmokucho <elmokuchoo@gmail.com>
2024-12-05 18:15:47 +0700

    fix error case sensitive at siswa model

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -17,7 +17,7 @@ class Siswa extends Model
 
     public function user ()
     {
-        return $this->belongsTo(user::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function orangtua ()
